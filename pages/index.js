724ac3b14fe4eb7405893122acb36e861a016bad
XianScripts <christfontanez@gmail.com>
2024-07-25 20:50:28 -0400

    rev1.1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-// pages/index.js
 import Head from 'next/head'
-import Link from 'next/link'
 import Header from '../components/Header'
 
 export default function Home() {
@@ -17,16 +15,14 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center flex-1 text-center p-4">
         <h1 className="text-6xl font-bold mt-8">Welcome to My Portfolio</h1>
         <p className="mt-3 text-2xl max-w-2xl">
-          Graduate TA at UCF | MS Business Analytics &apos;24 | BSBA Econ & Stats &apos;23
+          Graduate TA at UCF | MS Business Analytics '24 | BSBA Econ & Stats '23
         </p>
         <p className="mt-6 text-lg max-w-2xl">
           I am passionate about leveraging data to drive business insights and innovations. Explore my portfolio to see my experience and skills in data analysis, business economics, and more.
         </p>
-        <Link href="/about">
-          <a className="mt-8 px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
-            Learn More About Me
-          </a>
-        </Link>
+        <a href="/about" className="mt-8 px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
+          Learn More About Me
+        </a>
       </main>
 
       <footer className="w-full bg-gray-800 text-white p-4 flex justify-center">
@@ -35,3 +31,5 @@ export default function Home() {
     </div>
   )
 }
+
+
